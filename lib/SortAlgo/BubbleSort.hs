06ee43ce0e3b606a43c3b2dcbae30f08142ce20e
LukasK <ilja.kuklic@centrum.cz>
2020-01-02 09:46:23 +0000

    Make the trivial bubble sort cleaner

--- lib/SortAlgo/BubbleSort.hs
+++ lib/SortAlgo/BubbleSort.hs
@@ -9,9 +9,9 @@ import Data.Monoid
 -- Naive bubble sort.
 bubbleSortSimple :: SortAlgo
 bubbleSortSimple beg end = do
-    for_ [end,end-1..2] $ \stop ->
-        for_ [beg..(stop - 1)] $ \i ->
-            sort2 i (i + 1)
+    for_ [end-1,end-2..beg] $ \stop ->
+        for_ [beg..stop] $ \i ->
+            sort2 i (i+1)
 
 -- Bubble sort with a bit more sophisticated way of determining
 -- what part of the array shall be processed in each pass.
